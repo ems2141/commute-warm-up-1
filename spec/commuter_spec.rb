7b3c7de6a62ded0891bfc1405ad7a58df2340dcb
Ellie Schneiders <ems2141@gmail.com>
2014-06-23 09:33:17 -0600

    it returns two people in the report

--- spec/commuter_spec.rb
+++ spec/commuter_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/commuter'
 
 describe Commuter do
   it 'returns a report as a hash' do
+    pending
     csv_data = File.open('/Users/elsaschneiders/gSchoolWork/commute-warm-up-1/data/gschool_practice_data.csv')
 
     commute_hash = Commuter.new(csv_data).to_hash
@@ -15,6 +16,31 @@ describe Commuter do
         outbound: 15,
         distance: 0.65
     }]
+    }
+    expect(commute_hash).to eq expected_value
+  end
+
+  it 'two people ordered as a hash' do
+    csv_data = File.open('/Users/elsaschneiders/gSchoolWork/commute-warm-up-1/data/gschool_practice_data.csv')
+
+    commute_hash = Commuter.new(csv_data).to_hash
+
+    expected_value = {"Emily" =>[{
+                                     week: 4,
+                                     day: "Monday",
+                                     mode: "Walk",
+                                     inbound: 12,
+                                     outbound: 15,
+                                     distance: 0.65
+                                 }],
+                      "Gerard" => [{
+                                       week: 1,
+                                       day: "Wednesday",
+                                       mode: "Drive",
+                                       inbound: 14,
+                                       outbound: 12,
+                                       distance: 5
+                                   }]
     }
     expect(commute_hash).to eq expected_value
   end
