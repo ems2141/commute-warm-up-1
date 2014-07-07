c1b27c150a581259542b06ca7c991f89e4106d7d
Ellie Schneiders <ems2141@gmail.com>
2014-07-07 15:15:06 -0600

    file parser can return a sorted by week hash

--- lib/file_parser.rb
+++ lib/file_parser.rb
@@ -1,18 +1,46 @@
 class FileParser
 
-  def to_hash(data)
+  def initialize(data)
+    @data = data
+  end
+
+  def to_hash
+
     output = {}
-    data.each do |data|
-      output[data[0]] = {
-          week: data[1],
-          day: data[2],
-          mode: data[3],
-          inbound: data[4],
-          outbound: data[5],
-          distance: data[6]
-      }
+
+    @data.each do |data|
+
+      if output[data[0]].nil?
+        output[data[0]] = [{
+                               week: data[1],
+                               day: data[2],
+                               mode: data[3],
+                               inbound: data[4],
+                               outbound: data[5],
+                               distance: data[6]
+                           }]
+
+      else
+        output[data[0]] << {
+            week: data[1],
+            day: data[2],
+            mode: data[3],
+            inbound: data[4],
+            outbound: data[5],
+            distance: data[6]
+        }
+      end
+
     end
     output
   end
 
+  def sort_by_week
+    to_hash.each do |key, value|
+      value.sort_by! do |hash|
+        date_sort = Date.parse(hash[:day]).wday
+        [hash[:week], date_sort]
+      end
+    end
+  end
 end
--- spec/file_parser_spec.rb
+++ spec/file_parser_spec.rb
@@ -9,24 +9,90 @@ describe FileParser do
     ]
 
     actual = {
-        "Bob" => {
+        "Bob" => [{
             week: 2,
             day: "Wednesday",
             mode: "Drive",
             inbound: 30,
             outbound: 50,
             distance: 24
-        },
-        "Elsa" => {
+        }],
+        "Elsa" => [{
             week: 2,
             day: "Wednesday",
             mode: "Drive",
             inbound: 30,
             outbound: 50,
             distance: 24
-        }
+        }]
     }
 
-    expect(FileParser.new.to_hash(input)).to eq actual
+    expect(FileParser.new(input).to_hash).to eq actual
+  end
+
+  it 'can sort rows per person' do
+    input = [
+        ["Elsa", 2, "Wednesday", "Drive", 30, 50, 24],
+        ["Elsa", 1, "Wednesday", "Bike", 10, 20, 14],
+        ["Elsa", 1, "Tuesday", "Bike", 10, 20, 14],
+        ["Elsa", 1, "Monday", "Bike", 10, 20, 14],
+        ["Elsa", 1, "Friday", "Bike", 10, 20, 14],
+        ["Elsa", 1, "Thursday", "Bike", 10, 20, 14],
+    ]
+
+    actual = {
+        "Elsa" => [
+            {
+                week: 1,
+                day: "Monday",
+                mode: "Bike",
+                inbound: 10,
+                outbound: 20,
+                distance: 14
+            },
+            {
+                week: 1,
+                day: "Tuesday",
+                mode: "Bike",
+                inbound: 10,
+                outbound: 20,
+                distance: 14
+            },
+            {
+                week: 1,
+                day: "Wednesday",
+                mode: "Bike",
+                inbound: 10,
+                outbound: 20,
+                distance: 14
+            },
+            {
+                week: 1,
+                day: "Thursday",
+                mode: "Bike",
+                inbound: 10,
+                outbound: 20,
+                distance: 14
+            },
+            {
+                week: 1,
+                day: "Friday",
+                mode: "Bike",
+                inbound: 10,
+                outbound: 20,
+                distance: 14
+            },
+            {
+                week: 2,
+                day: "Wednesday",
+                mode: "Drive",
+                inbound: 30,
+                outbound: 50,
+                distance: 24
+            },
+        ]
+    }
+
+    expect(FileParser.new(input).sort_by_week).to eq(actual)
   end
 end
